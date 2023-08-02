e8dd212b7c8823b78e494e0c344a56fcf8856ebe
Connor Mackay <connor.mackay@gmail.com>
2023-08-02 12:04:13 -0400

    implement build workers method

--- learning_bot.py
+++ learning_bot.py
@@ -1,10 +1,19 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+from sc2.constants import NEXUS, PROBE, PYLON
+
 
 class r2_sc2(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
+        await self.build_workers()
+      
+    
+    async def build_workers(self):
+        for nexus in self.units(NEXUS).ready.noqueue:
+            if self.can_afford(PROBE):
+                await self.do(nexus.train(PROBE))
 
 
 run_game(maps.get("AbyssalReefLE"), [
