671c6f955bb0c0bee2e69e5ea37134d75e6dd8a1
Connor Mackay <connor.mackay@gmail.com>
2023-08-02 12:01:47 -0400

    implement boilerplate code

--- learning_bot.py
+++ learning_bot.py
@@ -1,3 +1,13 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
+
+class r2_sc2(sc2.BotAI):
+    async def on_step(self, iteration):
+        await self.distribute_workers()
+
+
+run_game(maps.get("AbyssalReefLE"), [
+    Bot(Race.Protoss, r2_sc2()),
+    Computer(Race.Terran, Difficulty.Easy)
+], realtime=False)
