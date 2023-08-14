73c7306d09cd31bae78c46a2c548ee706fc3201d
Connor Mackay <connor.mackay@gmail.com>
2023-08-14 11:23:19 -0400

    configure compile settings

--- sc2_neural_network.py
+++ sc2_neural_network.py
@@ -30,3 +30,19 @@ model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 
+## Connected Dense Layer
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+
+## Output Layer
+model.add(Dense(4, activation='softmax'))
+
+learning_rate = 0.0001
+opt = keras.optimizers.adam(lr=learning_rate, decay=1e-6)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+tensorboard = TensorBoard(log_dir='logs/stage1')
